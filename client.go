f158427b4f4d6337676cb84e7c602ec9d7eaecb5
sue445 <sue445@sue445.net>
2026-04-11 00:40:56 +0900

    Impl NewClient

--- client.go
+++ client.go
@@ -1,10 +1,17 @@
 package ghrcooldown
 
-import "time"
+import (
+	"net/http"
+	"time"
+
+	"github.com/cockroachdb/errors"
+	"github.com/google/go-github/v84/github"
+)
 
 // Client represents a client for interacting with the GitHub API with cooldown support.
 type Client struct {
-	token string
+	client      *github.Client
+	currentTime *time.Time
 }
 
 // ClientParams contains the configuration parameters required to initialize a new Client.
@@ -12,9 +19,24 @@ type ClientParams struct {
 	Token       string
 	BaseURL     string
 	CurrentTime *time.Time
+	HTTPClient  *http.Client
 }
 
 // NewClient creates and returns a new Client instance using the provided parameters.
 func NewClient(params *ClientParams) (*Client, error) {
-	return &Client{}, nil
+	client := github.NewClient(params.HTTPClient)
+
+	if params.Token != "" {
+		client = client.WithAuthToken(params.Token)
+	}
+
+	if params.BaseURL != "" {
+		var err error
+		client, err = client.WithEnterpriseURLs(params.BaseURL, "")
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+	}
+
+	return &Client{client: client, currentTime: params.CurrentTime}, nil
 }
