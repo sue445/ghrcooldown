7a84b6e60607e0d429d20366f5e7a837b9096a00
sue445 <sue445@sue445.net>
2026-04-15 23:37:30 +0900

    Add --version

--- cmd/ghrcooldown/main.go
+++ cmd/ghrcooldown/main.go
@@ -1,10 +1,41 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+
+	"github.com/cockroachdb/errors"
+	"github.com/sue445/ghrcooldown"
+	"github.com/urfave/cli/v3"
+)
+
 var (
 	// Revision represents app revision (injected from ldflags)
 	Revision string
 )
 
 func main() {
+	cmd := &cli.Command{
+		Name:    "ghrcooldown",
+		Version: fmt.Sprintf("%s (build. %s)", ghrcooldown.Version, Revision),
+		Usage:   "Fetch the latest GitHub Releases respecting the cooldown period.",
+	}
 
+	// Sort commands
+	sort.Slice(cmd.Commands, func(i, j int) bool {
+		return cmd.Commands[i].Name < cmd.Commands[j].Name
+	})
+
+	// Sort sub-command flags
+	for _, c := range cmd.Commands {
+		sort.Sort(cli.FlagsByName(c.Flags))
+	}
+
+	err := cmd.Run(context.Background(), os.Args)
+	if err != nil {
+		log.Fatal(errors.WithStack(err))
+	}
 }
