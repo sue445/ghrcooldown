6dcf2333c4448dc655b571bcdf92cd47779e500b
sue445 <sue445@sue445.net>
2026-04-16 23:10:14 +0900

    Add cmd/ghrcooldown/testutil_test.go

---
+++ cmd/ghrcooldown/testutil_test.go
@@ -0,0 +1,16 @@
+package main_test
+
+import (
+	"os"
+)
+
+// readTestData returns testdata
+func readTestData(filename string) string {
+	buf, err := os.ReadFile(filename)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return string(buf)
+}
